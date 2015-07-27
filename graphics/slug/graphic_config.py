5167414a6685bdc0e21ed1f8d9d197b6bf49363c
Sergio Hernandez <sergio@cerealcommas.com>
2015-07-27 13:08:26 -0400

    Bug fixes

--- graphics/slug/graphic_config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-COPY_GOOGLE_DOC_KEY = '1h4lDyTCGrn85l6MvwnS1QmrbPBwIRTJ8yYRM6JG0qQc'
-
-USE_ASSETS = False
-
-# Use these variables to override the default cache timeouts for this graphic
-# DEFAULT_MAX_AGE = 20
-# ASSETS_MAX_AGE = 300
